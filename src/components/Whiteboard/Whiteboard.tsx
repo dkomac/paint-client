e271b6052594669aa94bdcde2b159f74d940c950
unknown <komac91@msn.com>
2019-03-10 23:23:15 +0100

    fixed canvas and mouse positions, code need cleanup

--- src/components/Whiteboard/Whiteboard.tsx
+++ src/components/Whiteboard/Whiteboard.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 interface IpaintCord {
 	x: number;
 	y: number;
-	color: string;
+	color?: string;
 }
 
 interface IProps {
@@ -16,32 +16,84 @@ interface IState {
 }
 
 class Whiteboard extends React.Component<IProps, IState> {
-	private canvas: any;
+	private canvasRef: any;
 	private ctx: any;
 
 	constructor(props: any) {
 		super(props);
-		console.log(this.canvas);
 
-		this.canvas = React.createRef();
-		// this.ctx = this.canvas.current.getContext('2d');
+		this.canvasRef = React.createRef();
 	}
 
 	componentDidMount() {
-		console.log(this.ctx);
+		const canvas = this.canvasRef.current;
+		if (canvas) {
+			this.ctx = canvas.getContext('2d');
+		}
 	}
 
-	renderCanvas = (data: IpaintCord[]): void => {
-		console.log(data, this.canvas);
+	midPoint = (p1: any, p2: any) => {
+		return {
+			x: p1.x + (p2.x - p1.x) / 2,
+			y: p1.y + (p2.y - p1.y) / 2
+		};
+	};
+
+	fixCanvasPosition = (data: IpaintCord[]) => {
+		if (data) {
+			const newData = data.map(
+				(pos: IpaintCord): any => {
+					return {
+						x: pos.x - this.canvasRef.current.getBoundingClientRect().left,
+						y: pos.y - this.canvasRef.current.getBoundingClientRect().top,
+						color: pos.color
+					};
+				}
+			);
+
+			return newData;
+		}
+
+		return [];
+	};
+
+	renderCanvas = (paintData: IpaintCord[]): void => {
+		const data = this.fixCanvasPosition(paintData);
+		if (data.length < 1) {
+			return;
+		}
+		let p1 = data[0];
+		let p2 = data[1];
+
+		console.log(p1, p2);
+
+		this.ctx.beginPath();
+		this.ctx.moveTo(p1.x, p1.y);
+
+		for (let i = 1, len = data.length; i < len; i++) {
+			const midPoint = this.midPoint(p1, p2);
+			this.ctx.lineCap = 'round';
+			this.ctx.lineJoin = 'round';
+			this.ctx.lineWidth = 5; // data[i].settings.size;
+			this.ctx.shadowBlur = 2;
+			this.ctx.shadowColor = 'green'; // data[i].settings.color;
+			this.ctx.strokeStyle = 'green'; // data[i].settings.color;
+			this.ctx.quadraticCurveTo(p1.x, p1.y, midPoint.x, midPoint.y);
+			p1 = data[i];
+			p2 = data[i + 1];
+		}
+
+		this.ctx.stroke();
 	};
 
 	render() {
 		const { data } = this.props;
+
 		this.renderCanvas(data);
 		return (
 			<div>
 				<h1>canvas</h1>
-				<canvas ref={this.canvas} />
+				<canvas height="1000" width="1000" ref={this.canvasRef} />
 			</div>
 		);
 	}
